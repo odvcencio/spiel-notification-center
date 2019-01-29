db6665c4854e8ed2a39f9ec2952477ef06cd0add
Oscar Villavicencio <odvcencio@gmail.com>
2019-01-29 00:51:25 -0500

    editing handlers and routes

--- server/handlers.go
+++ server/handlers.go
@@ -9,25 +9,19 @@ import (
 	"time"
 
 	"github.com/labstack/echo"
-	"github.com/nsqio/go-nsq"
+	nsq "github.com/nsqio/go-nsq"
 )
 
-func handleCloudFlareMediaNotification(ctx echo.Context) error {
+func handleMuxMediaNotification(ctx echo.Context) error {
 	type Request struct {
-		UID            string `json:"uid"`
-		ThumbnailImage string `json:"thumbnail"`
-		ReadyToStream  bool   `json:"readyToStream"`
-		Status         struct {
-			State string `json:"state"`
-		} `json:"status"`
-		Meta       map[string]string `json:"meta"`
-		Labels     []string          `json:"labels"`
-		CreatedAt  time.Time         `json:"created"`
-		ModifiedAt time.Time         `json:"modified"`
-		Size       uint32            `json:"size"`
-		PreviewURL string            `json:"preview"`
+		Type   string `json:"type"`
+		ID     string `json:"id"`
+		Object struct {
+			Type string `json:"type"`
+			ID   string `json:"id"`
+		} `json:"object"`
+		CreatedAt time.Time `json:"created_at"`
 	}
-
 	// Read request body
 	reqData, err := ioutil.ReadAll(ctx.Request().Body)
 	if err != nil {
@@ -50,6 +44,46 @@ func handleCloudFlareMediaNotification(ctx echo.Context) error {
 
 	return nil
 }
+
+//
+// func handleCloudFlareMediaNotification(ctx echo.Context) error {
+// 	type Request struct {
+// 		UID            string `json:"uid"`
+// 		ThumbnailImage string `json:"thumbnail"`
+// 		ReadyToStream  bool   `json:"readyToStream"`
+// 		Status         struct {
+// 			State string `json:"state"`
+// 		} `json:"status"`
+// 		Meta       map[string]string `json:"meta"`
+// 		Labels     []string          `json:"labels"`
+// 		CreatedAt  time.Time         `json:"created"`
+// 		ModifiedAt time.Time         `json:"modified"`
+// 		Size       uint32            `json:"size"`
+// 		PreviewURL string            `json:"preview"`
+// 	}
+//
+// 	// Read request body
+// 	reqData, err := ioutil.ReadAll(ctx.Request().Body)
+// 	if err != nil {
+// 		log.Println(err)
+// 		return err
+// 	}
+//
+// 	// Unmarshal request json
+// 	var req Request
+// 	if err := json.Unmarshal(reqData, &req); err != nil {
+// 		println(err)
+// 		return err
+// 	}
+//
+// 	// TODO: Create appropriate Spiel and connect it to
+// 	//       appropriate User and Question
+//
+// 	// Responsing with OK
+// 	ctx.NoContent(200)
+//
+// 	return nil
+// }
 
 func handleTopicQuestionToUser(message *nsq.Message) error {
 	// Message model
--- server/routes.go
+++ server/routes.go
@@ -1,29 +1,26 @@
 package server
 
 import (
-	"log"
-	"os"
-	"spiel/notification-center/tools/cloudflare"
-
 	"github.com/labstack/echo"
 )
 
 func registerRoutes(e *echo.Echo) {
 	// Webhook Group
 	webhook := e.Group("/webhook")
+	webhook.POST("/videoUploaded", handleMuxMediaNotification)
 
-	// Registering CloudFlare Media Webhook
-	log.Println("Registering CloudFlare Media Webhook...")
-	cfClient := cloudflare.NewClient(
-		os.Getenv("CLOUDFLARE_ACCOUNT_ID"),
-		os.Getenv("CLOUDFLARE_EMAIL"),
-		os.Getenv("CLOUDFLARE_API_KEY"),
-	)
-	externalURL := os.Getenv("NOTIFICATION_CENTER_EXTERNAL_URL")
-	if relativePath, absoluteURL, err := cfClient.RegisterMediaWebhook(externalURL + "/webhook"); err != nil {
-		log.Fatalln(err)
-	} else {
-		log.Printf("CloudFlare Media Webhook registered successfully. (%v)\n", absoluteURL)
-		webhook.POST(relativePath, handleCloudFlareMediaNotification)
-	}
+	// // Registering CloudFlare Media Webhook
+	// log.Println("Registering CloudFlare Media Webhook...")
+	// cfClient := cloudflare.NewClient(
+	// 	os.Getenv("CLOUDFLARE_ACCOUNT_ID"),
+	// 	os.Getenv("CLOUDFLARE_EMAIL"),
+	// 	os.Getenv("CLOUDFLARE_API_KEY"),
+	// )
+	// externalURL := os.Getenv("NOTIFICATION_CENTER_EXTERNAL_URL")
+	// if relativePath, absoluteURL, err := cfClient.RegisterMediaWebhook(externalURL + "/webhook"); err != nil {
+	// 	log.Fatalln(err)
+	// } else {
+	// 	log.Printf("CloudFlare Media Webhook registered successfully. (%v)\n", absoluteURL)
+	// 	webhook.POST(relativePath, handleCloudFlareMediaNotification)
+	// }
 }
