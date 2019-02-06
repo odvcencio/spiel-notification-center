a1f470648e3c598c38000c770c2b7d9d7d5cb5d6
Oscar Villavicencio <odvcencio@gmail.com>
2019-02-06 01:48:45 -0500

    editing handlers for incoming webhoooks

--- server/handlers.go
+++ server/handlers.go
@@ -2,8 +2,10 @@ package server
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"spiel/notification-center/database"
 	"time"
 
 	"github.com/labstack/echo"
@@ -30,18 +32,28 @@ func handleMuxMediaNotification(ctx echo.Context) error {
 	// Unmarshal request json
 	var dict echo.Map
 	if err := json.Unmarshal(reqData, &dict); err != nil {
-		println(err)
+		log.Println(err)
 		return err
 	}
 
-	log.Println(dict)
+	webhookType := dict["type"].(string)
 
-	// TODO: Create appropriate Spiel and connect it to
-	//       appropriate User and Question
+	if webhookType == "video.asset.ready" {
+		videoID := dict["object"].(echo.Map)["id"].(string)
 
-	//database.UpdateSpielWithVideoURL(req.ID, videoID)
-	// Responsing with OK
-	ctx.NoContent(200)
+		dataMap := dict["data"].(echo.Map)
+		playbackIDArray := dataMap["playback_ids"].([]echo.Map)
+		playbackIDObject := playbackIDArray[0]
+		playbackID := playbackIDObject["id"].(string)
+
+		videoURL := fmt.Sprintf("https://stream.mux.com/%s.m3u8", playbackID)
+
+		if err := database.UpdateSpielWithVideoURL(videoID, videoURL); err != nil {
+			log.Println(err)
+		}
+
+		//send notification
+	}
 
 	return nil
 }
